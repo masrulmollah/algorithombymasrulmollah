16b60fbf559706151d3b5fc9e7136113c9104dd0
masrulmollah <54163010+masrulmollah@users.noreply.github.com>
2019-09-30 18:36:09 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-<!doctype html>
-<html>
-	<head>
-		<title> Variables , Arrays </title>
-		<script>
-			var car = "honda";
-		</script>
-	</head>
-	<body>
-		<script>
-			document.write("anything");
-		</script>
-	</body>
-</html>
+
 
 
