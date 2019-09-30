bcfd5500cdf6b162740e356fb17e6a54f300e011
masrulmollah <54163010+masrulmollah@users.noreply.github.com>
2019-09-30 22:39:12 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,2 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title> Untitled Document<title/>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/oRkNaF0QvnI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</head>
-
-<body>
-<div id="container">
-<ul>
-<li>Home</li>
-<li>About</li>
-</ul>
-</div>
-</body>
-</html>
 
 
